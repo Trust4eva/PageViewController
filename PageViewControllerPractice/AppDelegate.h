71502716423f5a9b6ab414d385820fd901f3f054
Trustin Harris <trust4eva@fb.com>
2018-07-23 11:42:58 -0700

    Initial Commit

---
+++ PageViewControllerPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewControllerPractice
+//
+//  Created by Trustin Harris on 7/23/18.
+//  Copyright © 2018 xToBeAnnounce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
